f9fc6b525deeac3e82324aa7f1f793c1d3e6bb76
Moulimense <achachisolayman15@gmail.com>
2025-09-26 12:05:15 +0200

    commit initial du projet

---
+++ Test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("modele/ModeleFront.php");
+
+$modele = new ModeleFront();
+
+var_dump($modele->getLesCategories());
+
+?>
---
+++ config/.config.php
@@ -0,0 +1,6 @@
+<?php
+define('USER','root');
+define('PASSWD','');
+define('SERVER','localhost:3307');
+define('BASE','gsb_param');
+?>
